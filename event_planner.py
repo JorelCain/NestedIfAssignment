bfe41049dea6d7b5263751ef81a3afd8c300685f
JorelCain <jorelcain2@gmail.com>
2024-07-17 12:13:06 -0700

    Changed to inline statement"

--- event_planner.py
+++ event_planner.py
@@ -3,7 +3,4 @@ venue = "large hall" if attendees > 100 else "conference room"
 print(venue)
 
 vegetarian = input("Would you like vegetarian food?")
-if vegetarian == "yes":
-    print("Veggie delight caterers is the best place for vegetarian food")
-elif vegetarian == "no":
-    print("Gourmet Meals Caterers is the best choice for you")
+print("Veggie Delight Caterers") if vegetarian == "yes" else print("Gourmet Meals Caterers")
